6752bdee3ae8c29f5527113fbf1d59ea0accc7bd
Jesse Himmelstein <jesse@heavenstone.net>
2016-07-04 23:34:01 +0200

    Moved config to separate file

---
+++ config.sample.js
@@ -0,0 +1,2 @@
+var API_KEY = "ea50f2be9a7fe2c72da17081fc78af9e";
+var API_SECRET = "fUee4SrXxNb1KRC_0acJjZDChxhQ7f7t";
--- popup.js
+++ popup.js
@@ -62,9 +62,6 @@ function renderResults() {
   document.getElementById('total-count').innerText = currentResults.total;
 }
 
-var API_KEY = "ea50f2be9a7fe2c72da17081fc78af9e";
-var API_SECRET = "fUee4SrXxNb1KRC_0acJjZDChxhQ7f7t";
-
 var currentResults = {
   male: 0,
   female: 0,
