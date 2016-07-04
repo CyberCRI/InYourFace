c823d3ea92ea8c802b2d09602531c00f81d61a43
Jesse Himmelstein <jesse@heavenstone.net>
2016-07-04 22:51:49 +0200

    Can analyze page

--- popup.js
+++ popup.js
@@ -52,24 +52,52 @@ function renderStatus(statusText) {
   document.getElementById('status').textContent = statusText;
 }
 
+function changeImage(url) {
+  document.getElementById('photo').src = url;
+}
+
+function renderResults() {
+  document.getElementById('male-count').innerText = currentResults.male;
+  document.getElementById('female-count').innerText = currentResults.female;
+  document.getElementById('total-count').innerText = currentResults.total;
+}
+
 var API_KEY = "ea50f2be9a7fe2c72da17081fc78af9e";
 var API_SECRET = "fUee4SrXxNb1KRC_0acJjZDChxhQ7f7t";
+
+var currentResults = {
+  male: 0,
+  female: 0,
+  total: 0
+}
 
 function analyzeImage(imageUrl) {
   var faceDetectUrl = "https://apius.faceplusplus.com/v2/detection/detect?api_key=" + API_KEY + "&api_secret=" + API_SECRET + "&url=" + imageUrl;
   return $.getJSON(faceDetectUrl);
 }
 
+function updateResults(results) {
+  currentResults.total++;
+
+  if(results.face.length == 0) return;
+
+  if(results.face[0].attribute.gender.value == "Male") currentResults.male++;
+  else currentResults.female++;
+}
+
 chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
   chrome.tabs.sendMessage(tabs[0].id, { command: "analyze" }, function(response) {
-    renderStatus("found " + response.imageUrls.length + " images");
+    renderStatus("Found " + response.imageUrls.length + " images");
 
     // Request one at a time
     var promise = analyzeImage(response.imageUrls[0]);
     for(var i = 1; i < response.imageUrls.length; i++) {
       (function(i) {
-        promise = promise.then(function() { 
-          renderStatus("analyzing image " + i + " of " + imageUrls.length);
+        promise = promise.then(function(results) { 
+          updateResults(results);
+          renderResults();
+          renderStatus("analyzing image " + i + " of " + response.imageUrls.length);
+          changeImage(response.imageUrls[i]);
           return analyzeImage(response.imageUrls[i])
         });
       })(i);
@@ -77,84 +105,11 @@ chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
 
     promise.then(function(results) {
       console.log("analysis done"); 
-      renderStatus("got " + results.length + " responses back");
+      updateResults(results);
+      renderResults();
+      changeImage("");
+      renderStatus("Done with page");
     }); 
-
-    /*var req = new XMLHttpRequest();
-    req.open("GET", faceDetectUrl, false);
-    req.send(null);
-    if(req.status == 200)
-      console.log("Got response", req.responseText);*/
-
-
   });
 });
 
-
-/**
- * @param {string} searchTerm - Search term for Google Image search.
- * @param {function(string,number,number)} callback - Called when an image has
- *   been found. The callback gets the URL, width and height of the image.
- * @param {function(string)} errorCallback - Called when the image is not found.
- *   The callback gets a string that describes the failure reason.
- */
-/*function getImageUrl(searchTerm, callback, errorCallback) {
-  // Google image search - 100 searches per day.
-  // https://developers.google.com/image-search/
-  var searchUrl = 'https://ajax.googleapis.com/ajax/services/search/images' +
-    '?v=1.0&q=' + encodeURIComponent(searchTerm);
-  var x = new XMLHttpRequest();
-  x.open('GET', searchUrl);
-  // The Google image search API responds with JSON, so let Chrome parse it.
-  x.responseType = 'json';
-  x.onload = function() {
-    // Parse and process the response from Google Image Search.
-    var response = x.response;
-    if (!response || !response.responseData || !response.responseData.results ||
-        response.responseData.results.length === 0) {
-      errorCallback('No response from Google Image search!');
-      return;
-    }
-    var firstResult = response.responseData.results[0];
-    // Take the thumbnail instead of the full image to get an approximately
-    // consistent image size.
-    var imageUrl = firstResult.tbUrl;
-    var width = parseInt(firstResult.tbWidth);
-    var height = parseInt(firstResult.tbHeight);
-    console.assert(
-        typeof imageUrl == 'string' && !isNaN(width) && !isNaN(height),
-        'Unexpected respose from the Google Image Search API!');
-    callback(imageUrl, width, height);
-  };
-  x.onerror = function() {
-    errorCallback('Network error.');
-  };
-  x.send();
-}*/
-
-
-/*
-document.addEventListener('DOMContentLoaded', function() {
-  getCurrentTabUrl(function(url) {
-    // Put the image URL in Google search.
-    renderStatus('Performing Google Image search for ' + url);
-
-    getImageUrl(url, function(imageUrl, width, height) {
-
-      renderStatus('Search term: ' + url + '\n' +
-          'Google image search result: ' + imageUrl);
-      var imageResult = document.getElementById('image-result');
-      // Explicitly set the width/height to minimize the number of reflows. For
-      // a single image, this does not matter, but if you're going to embed
-      // multiple external images in your page, then the absence of width/height
-      // attributes causes the popup to resize multiple times.
-      imageResult.width = width;
-      imageResult.height = height;
-      imageResult.src = imageUrl;
-      imageResult.hidden = false;
-
-    }, function(errorMessage) {
-      renderStatus('Cannot display image. ' + errorMessage);
-    });
-  });
-});*/
